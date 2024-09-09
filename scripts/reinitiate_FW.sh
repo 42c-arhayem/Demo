809479bb37397017f9fd992d2a9a37a796f91348
Ali Rhayem <ali.rhayem@42crunch.com>
2024-09-09 11:40:55 +0100

    FW reset

---
+++ scripts/reinitiate_FW.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Prompt the user for the namespace
+echo "Please enter the namespace:"
+read namespace
+echo "Enter the state of the API (vulnerable/patched):\n 1- Vulnerable\n 2- Patched"
+read api_state
+
+helm install firewall-props --debug ./artifacts/42c-firewall --namespace ali --set-string apifirewall.protection_token=d7cdec1f-9301-4042-b509-c6bb836504c4
+
+if api_state == "1"; then
+    helm upgrade  pixiapi --debug ./artifacts/42c-pixiapi --namespace $namespace --set pixiapp.inject_firewall=true --set pixiapp.pod_listen_port=8080 -f ./artifacts/42c-pixiapi/values-api.yaml
+elif api_state == "2"; then
+    helm upgrade  pixiapi --debug ./artifcats/42c-pixiapi --namespace $namespace --set pixiapp.inject_firewall=true --set pixiapp.pod_listen_port=8080 -f ./artifacts/42c-pixiapi/values-patched.yaml
+else
+    echo "Invalid choice."
+    exit 1
+fi
